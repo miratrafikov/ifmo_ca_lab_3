2fd181b26a1d951d14edbeb5baf8995103d1219b
mirat <miratrafikov@yandex.ru>
2020-02-23 19:04:17 +0300

    Empty

---
+++ ifmo_ca_lab_3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ifmo_ca_lab_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
