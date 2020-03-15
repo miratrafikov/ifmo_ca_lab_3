e192640bf9f8ebcacf6e02634aefe3ed494df3ad
Mirat Rafikov <miratworking@gmail.com>
2020-03-16 02:24:34 +0300

    sd

--- test/Evaluation.Test/UContext.cs
+++ test/Evaluation.Test/UContext.cs
@@ -13,6 +13,9 @@ namespace ShiftCo.ifmo_ca_lab_3.Evaluation.Test
         {
             var obj_1 = new Value(1);
             var obj_2 = new Value(2);
+            Debug.WriteLine($"obj_1 hash = {obj_1.GetHashCode()}");
+            Debug.WriteLine($"obj_2 hash = {obj_2.GetHashCode()}");
+            Debug.WriteLine($"new Value(1) hash = {(new Value(1)).GetHashCode()}");
             Context.AddEntry(obj_1, obj_2);
             Debug.WriteLine($"Key ({Context.GetSubstitute(new Value(1)).Key}).");
         }
