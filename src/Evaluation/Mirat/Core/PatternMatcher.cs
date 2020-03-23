863b99cca567055db5c6b56d9bb428225c0a5c4e
Anton Orekhovskiy <anton.o.0619@gmail.com>
2020-03-24 02:57:54 +0300

    init

---
+++ src/Evaluation/Mirat/Core/PatternMatcher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShiftCo.ifmo_ca_lab_3.Evaluation.Mirat.Core
+{
+    class PatternMatcher
+    {
+        public static bool  Matches(object pattern, object obj)
+        {
+            return default;
+        } 
+    }
+}
