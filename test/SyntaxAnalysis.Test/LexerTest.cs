68d77a9b39681051e95376666c3906f21e80ccff
Mirat Rafikov <miratworking@gmail.com>
2020-03-26 12:13:57 +0300

    Unit test

---
+++ test/SyntaxAnalysis.Test/LexerTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShiftCo.ifmo_ca_lab_3.SyntaxAnalysis.Lexington;
+
+namespace ShiftCo.ifmo_ca_lab_3.SyntaxAnalysisTest
+{
+    [TestClass]
+    public class LexerTest
+    {
+        [TestMethod]
+        public void Tokenize_OnePattern()
+        {
+            var tokens = Lexer.Tokenize("abc_");
+            foreach (var token in tokens)
+            {
+                Debug.WriteLine($"{token.Type} : {token.Content}");
+            }
+        }
+    }
+}
