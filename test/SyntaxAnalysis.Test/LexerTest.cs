8e8f553355efe6bbaee89cfbd3acb6b8fa726c80
Mirat Rafikov <miratworking@gmail.com>
2020-03-26 12:42:53 +0300

    add another unit

--- test/SyntaxAnalysis.Test/LexerTest.cs
+++ test/SyntaxAnalysis.Test/LexerTest.cs
@@ -11,10 +11,22 @@ namespace ShiftCo.ifmo_ca_lab_3.SyntaxAnalysisTest
         public void Tokenize_OnePattern()
         {
             var tokens = Lexer.Tokenize("abc_");
-            foreach (var token in tokens)
-            {
-                Debug.WriteLine($"{token.Type} : {token.Content}");
-            }
+            Assert.AreEqual("abc", tokens[0].Content);
+            Assert.AreEqual("_", tokens[1].Content);
+        }
+
+        [TestMethod]
+        public void Tokenize_OnePatternInExpression()
+        {
+            var tokens = Lexer.Tokenize("expr(a_b,69)");
+            Assert.AreEqual("expr", tokens[0].Content);
+            Assert.AreEqual("(", tokens[1].Content);
+            Assert.AreEqual("a", tokens[2].Content);
+            Assert.AreEqual("_", tokens[3].Content);
+            Assert.AreEqual("b", tokens[4].Content);
+            Assert.AreEqual(",", tokens[5].Content);
+            Assert.AreEqual("69", tokens[6].Content);
+            Assert.AreEqual(")", tokens[7].Content);
         }
     }
 }
