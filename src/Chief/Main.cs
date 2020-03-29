a22e2c26a32ebee1a143a2348628aa7312adec1f
Mirat <miratrafikov@yandex.ru>
2020-03-29 08:06:06 +0300

    Exceptions and overrides in Main.cs

--- src/Chief/Main.cs
+++ src/Chief/Main.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using ShiftCo.ifmo_ca_lab_3.Evaluation.Interfaces;
 using ShiftCo.ifmo_ca_lab_3.SyntaxAnalysis.Lexington;
 using ShiftCo.ifmo_ca_lab_3.SyntaxAnalysis.Parseltongue;
@@ -22,16 +23,40 @@ namespace ShiftCo.ifmo_ca_lab_3.Chief
 
         public static void GetTokensFromString()
         {
+            if (string.IsNullOrEmpty(s_inputString))
+            {
+                throw new Exception("String is not provided for processing.");
+            }
+            s_tokens = Lexer.Tokenize(InputString);
+        }
+
+        public static void GetTokensFromString(string inputString)
+        {
+            InputString = inputString;
             s_tokens = Lexer.Tokenize(InputString);
         }
 
         public static void GetTreeFromTokens()
         {
+            if (s_tokens == null)
+            {
+                throw new Exception("No generated tokens list exists.");
+            }
             s_tree = Parser.Parse(s_tokens);
         }
 
         public static void GetTreeFromString()
         {
+            if (string.IsNullOrEmpty(s_inputString))
+            {
+                throw new Exception("String is not provided for processing.");
+            }
+            s_tree = Parser.Parse(Lexer.Tokenize(InputString));
+        }
+        
+        public static void GetTreeFromString(string inputString)
+        {
+            InputString = inputString;
             s_tree = Parser.Parse(Lexer.Tokenize(InputString));
         }
 
