6b047f431cc770b4574085cbae3aacca18819176
Mirat <miratrafikov@yandex.ru>
2020-04-01 15:37:13 +0300

    Remove unused from atoms

--- src/Evaluation/Types/Atoms/Symbol.cs
+++ src/Evaluation/Types/Atoms/Symbol.cs
@@ -13,15 +13,9 @@ namespace ShiftCo.ifmo_ca_lab_3.Evaluation.Types.Atoms
 
         public static implicit operator Symbol(string value) => new Symbol(value);
 
-        public static bool operator ==(Symbol left, Symbol right) =>
-            string.Compare(left.Value, right.Value) == 0;
-
-        public static bool operator !=(Symbol left, Symbol right) =>
-            string.Compare(left.Value, right.Value) != 0;
-
         public object Clone()
         {
-            return this.MemberwiseClone();
+            return MemberwiseClone();
         }
     }
 }
