606c8e664b0ca09d11664ebb212b24efd09b06e3
Mirat <miratrafikov@yandex.ru>
2020-04-01 16:02:39 +0300

    Elaborated on patterns

--- src/Evaluation/Types/Pattern.cs
+++ src/Evaluation/Types/Pattern.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using ShiftCo.ifmo_ca_lab_3.Evaluation.Interfaces;
 using ShiftCo.ifmo_ca_lab_3.Evaluation.Types.Atoms;
 
@@ -9,12 +7,14 @@ namespace ShiftCo.ifmo_ca_lab_3.Evaluation.Types
     public class Pattern : IElement
     {
         public Symbol Name { get; }
-        public IElement Type { get; }
+        public Type Type { get; }
+        public bool IsSequence { get; }
 
-        public Pattern(Symbol name, IElement type)
+        public Pattern(Symbol name, Type type, bool isSequence)
         {
             Name = name;
             Type = type;
+            IsSequence = isSequence;
         }
 
         public object Clone()
