135df34763320d2bb7035aad81161cb1b47dddff
Mirat <miratrafikov@yandex.ru>
2020-04-06 09:08:17 +0300

    Expression class

---
+++ src/NewEvaluation/Types/Expression.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ShiftCo.ifmo_ca_lab_3.NewEvaluation.Interfaces;
+
+namespace ShiftCo.ifmo_ca_lab_3.NewEvaluation.Types
+{
+    public class Expression : IElement
+    {
+        public IElement Head { get; set; }
+        public List<IElement> Elements { get; set; }
+
+        public Expression(IElement head, List<IElement> elements)
+        {
+            Head = head;
+            Elements = elements;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 17 * Head.GetHashCode();
+            foreach (var element in Elements)
+            {
+                hashCode += 23 * element.GetHashCode();
+            }
+
+            return hashCode;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            // TODO Implement Equals() for Expression
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            var str = Head + "[";
+            foreach (var element in Elements)
+            {
+                str += element.ToString();
+                if (element.Equals(Elements.Last()))
+                {
+                    str += "]";
+                }
+                else
+                {
+                    str += ", ";
+                }
+            }
+            return str;
+        }
+    }
+}
