855f6e621403b42388dad603ce9421ac26becec3
Mirat <miratrafikov@yandex.ru>
2020-04-06 07:03:42 +0300

    Atom types

---
+++ src/NewEvaluation/Types/Atoms/Integer.cs
@@ -0,0 +1,12 @@
+﻿namespace ShiftCo.ifmo_ca_lab_3.NewEvaluation.Types.Atoms
+{
+    public class Integer
+    {
+        public readonly int Value;
+
+        public Integer(int value)
+        {
+            Value = value;
+        }
+    }
+}
---
+++ src/NewEvaluation/Types/Atoms/Symbol.cs
@@ -0,0 +1,12 @@
+﻿namespace ShiftCo.ifmo_ca_lab_3.NewEvaluation.Types.Atoms
+{
+    public class Symbol
+    {
+        public readonly string Value;
+
+        public Symbol(string value)
+        {
+            Value = value;
+        }
+    }
+}
