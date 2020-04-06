b5b54b9585327e1adabe7815fe7ce7563bdc2ec0
Mirat <miratrafikov@yandex.ru>
2020-04-06 09:35:26 +0300

    Pattern class

---
+++ src/NewEvaluation/Types/Pattern.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ShiftCo.ifmo_ca_lab_3.NewEvaluation.Interfaces;
+using ShiftCo.ifmo_ca_lab_3.NewEvaluation.Types.Atoms;
+
+namespace ShiftCo.ifmo_ca_lab_3.NewEvaluation.Types
+{
+    public class Pattern : IElement
+    {
+        public Symbol Name { get; }
+        public Type Type { get; }
+        public bool IsSequence { get; }
+
+        public Pattern(Symbol name, Type type, bool isSequence)
+        {
+            Name = name;
+            Type = type;
+            IsSequence = isSequence;
+        }
+    }
+}
