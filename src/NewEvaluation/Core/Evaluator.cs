6146b8aca727e53309a9d6a50db3ae17cea6cd13
Mirat <miratrafikov@yandex.ru>
2020-04-10 06:27:28 +0300

    Generic Evaluate method

--- src/NewEvaluation/Core/Evaluator.cs
+++ src/NewEvaluation/Core/Evaluator.cs
@@ -2,6 +2,8 @@
 using ShiftCo.ifmo_ca_lab_3.Commons;
 using ShiftCo.ifmo_ca_lab_3.Commons.Exceptions;
 using ShiftCo.ifmo_ca_lab_3.NewEvaluation.Interfaces;
+using ShiftCo.ifmo_ca_lab_3.NewEvaluation.Types;
+using ShiftCo.ifmo_ca_lab_3.NewEvaluation.Types.Atoms;
 
 namespace ShiftCo.ifmo_ca_lab_3.NewEvaluation.Core
 {
@@ -13,17 +15,55 @@ namespace ShiftCo.ifmo_ca_lab_3.NewEvaluation.Core
         public static IElement Run(IElement element)
         {
             s_evaluationRuns = 0;
-            var result = Evaluate(element);
-            if (result.Success)
-            {
-                return (IElement)result.Value;
-            }
-            throw new TooManyIterationsException();
+            return Evaluate(element);
         }
 
-        private static Result Evaluate(IElement element)
+        private static IElement Evaluate(IElement elementToEvaluate)
+        {
+            if (s_evaluationRuns == EvaluationRunsLimit)
+            {
+                throw new TooManyIterationsException();
+            }
+            IElement evaluatedElement;
+            switch (elementToEvaluate)
+            {
+                case Integer elementAsInteger:
+                    evaluatedElement = EvaluateInteger(elementAsInteger);
+                    break;
+                case Symbol elementAsSymbol:
+                    evaluatedElement = EvaluateSymbol(elementAsSymbol);
+                    break;
+                case Expression elementAsExpression:
+                    evaluatedElement = EvaluateExpression(elementAsExpression);
+                    break;
+                default:
+                    return elementToEvaluate;
+            }
+
+            s_evaluationRuns++;
+            return ElementNotChanged(elementToEvaluate, evaluatedElement)
+                ? evaluatedElement
+                : Evaluate(evaluatedElement);
+        }
+
+        private static IElement EvaluateInteger(Integer integer)
         {
             throw new NotImplementedException();
+        }
+
+        private static IElement EvaluateSymbol(Symbol symbol)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static IElement EvaluateExpression(Expression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static bool ElementNotChanged(IElement initialElement, IElement evaluatedElement)
+        {
+            return initialElement.Equals(evaluatedElement);
         }
     }
 }
