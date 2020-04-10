402c2f5e262796112c8b940ac63ecf21db61f132
Mirat <miratrafikov@yandex.ru>
2020-04-10 08:31:47 +0300

    PatternMatcher class

---
+++ src/NewEvaluation/Core/PatternMatcher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ShiftCo.ifmo_ca_lab_3.Commons;
+using ShiftCo.ifmo_ca_lab_3.NewEvaluation.Interfaces;
+using ShiftCo.ifmo_ca_lab_3.NewEvaluation.Types.Atoms;
+
+namespace ShiftCo.ifmo_ca_lab_3.NewEvaluation.Core
+{
+    public static class PatternMatcher
+    {
+        public static Result TryMatch(IElement element, IElement lhs)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
