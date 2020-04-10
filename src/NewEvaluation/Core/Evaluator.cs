b9d354c2e7f65a45fa013f87869a19813611f8a3
Mirat <miratrafikov@yandex.ru>
2020-04-10 06:03:39 +0300

    Evaluator draft

---
+++ src/NewEvaluation/Core/Evaluator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using ShiftCo.ifmo_ca_lab_3.Commons;
+using ShiftCo.ifmo_ca_lab_3.Commons.Exceptions;
+using ShiftCo.ifmo_ca_lab_3.NewEvaluation.Interfaces;
+
+namespace ShiftCo.ifmo_ca_lab_3.NewEvaluation.Core
+{
+    public static class Evaluator
+    {
+        private const int EvaluationRunsLimit = 100000;
+        private static int s_evaluationRuns;
+
+        public static IElement Run(IElement element)
+        {
+            s_evaluationRuns = 0;
+            var result = Evaluate(element);
+            if (result.Success)
+            {
+                return (IElement)result.Value;
+            }
+            throw new TooManyIterationsException();
+        }
+
+        private static Result Evaluate(IElement element)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
