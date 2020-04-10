1f9fcd18ec667ec1259de9dc32d0664f53fa925e
Mirat <miratrafikov@yandex.ru>
2020-04-10 10:31:58 +0300

    TryMatch definitions

--- src/NewEvaluation/Core/PatternMatcher.cs
+++ src/NewEvaluation/Core/PatternMatcher.cs
@@ -11,6 +11,13 @@ namespace ShiftCo.ifmo_ca_lab_3.NewEvaluation.Core
     {
         public static Result TryMatch(IElement element, IElement lhs)
         {
+            var elementAsList = new List<IElement> { element };
+            var lhsAsList = new List<IElement> { lhs };
+            return TryMatch(elementAsList, lhsAsList);
+        }
+
+        private static Result TryMatch(List<IElement> elements, List<IElement> patterns)
+        {
             throw new NotImplementedException();
         }
     }
